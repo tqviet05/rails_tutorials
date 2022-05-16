cd037f56f338ee4152b7a43795e04afb3c592c5c
tqviet05 <tt.viet.tang@tomosia.com>
2022-05-16 14:39:39 +0700

    Chapter_9 Done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
+  # Tao dang nhap
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me].to_i.zero? ? forget(user): remember(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -13,8 +14,9 @@ class SessionsController < ApplicationController
     end
   end
 
+  # Thoat dang nhap
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  # Tao tk
   def create
     @user = User.new(user_params)
     if @user.save 
